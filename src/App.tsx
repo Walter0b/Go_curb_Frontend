9cf5df135561b9fc821a256958eb256f6132f878
walter0b <jakinjoram5@outlook.com>
2023-11-03 13:54:29 +0100

    add background

--- src/App.tsx
+++ src/App.tsx
@@ -209,7 +209,7 @@ function App() {
             {/* Buttons and dropdowns here */}
             <button id="dropdownRadioButton" data-dropdown-toggle="dropdownRadio" className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
 
-              Last 30 days
+              Filter
               <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
               </svg>

09268dd91817b34d3c5221d25cb9bfe82487bfae
Walter0b <69851744+Walter0b@users.noreply.github.com>
2023-12-11 11:07:41 +0100

    init project

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path";
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-
-    alias: {
-
-      "@": path.resolve(__dirname, "./src/"),
-      routes: `${path.resolve(__dirname, "./src/routes/")}`,
-      services: `${path.resolve(__dirname, "./src/services/")}`,
-
-    }
-
-  }
 })

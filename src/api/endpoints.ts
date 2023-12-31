e5044bfcf1f8101880e5cdb6d38c748f90a9d2ef
Walter0b <69851744+Walter0b@users.noreply.github.com>
2023-12-31 18:40:39 +0100

    feat : update endpoint

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -3,7 +3,7 @@ const baseEndpoint = import.meta.env.VITE_API_BASE_URL;
 
 export const apiEndpoints = {
   base: baseEndpoint,
-  customers: '/customers',
-  invoices: '/invoices',
-  payments: '/payments',
+  customers: 'customers',
+  invoices: 'invoices',
+  payments: 'payments',
 };

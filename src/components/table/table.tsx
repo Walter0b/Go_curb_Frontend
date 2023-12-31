2458caebc06fa8c60f7c5c1b3624dc06b3a325e3
Walter0b <69851744+Walter0b@users.noreply.github.com>
2023-12-31 18:45:36 +0100

    feat : create test data

---
+++ src/components/table/table.tsx
@@ -0,0 +1,28 @@
+import TableItem from "./tableItem";
+
+export const Table = () => {
+    const data = [
+        { ID: 1, Customer_name: 'John Doe', City: 'New York', /* ... other fields ... */ },
+        { ID: 2, Customer_name: 'Jane Doe', City: 'Los Angeles', /* ... other fields ... */ },
+    ];
+
+    const columns = [
+        { key: 'ID', label: 'ID' },
+        { key: 'Customer_name', label: 'Customer Name' },
+        { key: 'City', label: 'City' },
+    ];
+
+    const handleEdit = (item) => {
+        console.log('Edit', item);
+    };
+
+    const handleDelete = (item) => {
+        console.log('Delete', item);
+    };
+
+    return (
+        <TableItem data={data} columns={columns} onEdit={handleEdit} onDelete={handleDelete} />
+    );
+};
+
+export default Table;

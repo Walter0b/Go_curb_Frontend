d57c56721427957fb0706b25089674b4b6586ae3
Walter0b <69851744+Walter0b@users.noreply.github.com>
2023-12-31 18:44:49 +0100

    feat : update routes

--- src/pages/Router/Router.tsx
+++ src/pages/Router/Router.tsx
@@ -1,17 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 // import App from "../../App";
 import Login from "@components/login/login";
-import Customers from "@pages/Admin/Customer/ListCustomer";
+
+import Modal from "@components/modal";
+import DynamicForm from "@components/modal";
+import Fields from "@components/modal/mainmodal";
+import MyComponent from "@components/test";
+import Customers from "@pages/Admin/Customer";
 import Layout from "@components/layout";
-import TestComponent from "@components/test";
 
 export const appRouters = createBrowserRouter([
     {
         path: '/',
-        element: <TestComponent></TestComponent>,
+        element: <Layout></Layout>,
         children: [
             { path: "", element: <Login />, },
             { path: "customers", element: <Customers /> },
+            { path: "test", element: <MyComponent /> },
         ]
     }
 ]);
